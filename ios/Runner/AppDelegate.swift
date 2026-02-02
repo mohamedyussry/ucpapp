405904e91e488dd3b1da28a7fa5b04d36b2bebc1
Mohammed <mohammed.yussry@gmail.com>
2026-02-02 19:19:42 +0200

    "Fix_iOS_Map_Crash_by_adding_Google_Maps_API_Key_to_AppDelegate"

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import GoogleMaps
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,7 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyBGxQwI9bZy3GPmAIzwGXV5m2gs3ob2igo")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
